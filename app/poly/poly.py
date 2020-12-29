3dacb506d91a603591b1aed691976da763f76722
Mike Nisbet <nisbet.mr@gmail.com>
2020-12-29 16:07:25 +0000

    Poly /assets/ wrapper

--- app/poly/poly.py
+++ app/poly/poly.py
@@ -1,14 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import requests
+import json
+
+with open("config.json") as config_file:
+    data = json.load(config_file)
+    print(data["poly_api_key"])
 
 router = APIRouter(
     prefix="/poly",
-    tags=["poly"])
+    tags=["Poly"]
+    )
 
 
-@router.get("/assets", tags=["poly"])
+@router.get("/assets")
 async def get_poly_assets_list():
-    return 1
+    r = requests.get(f'https://poly.googleapis.com/v1/assets?key={data["poly_api_key"]}')
+    print(r.status_code)
+    if (r.status_code == 200):
+        return r.json()
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-@router.get("/assets/{asset}", tags=["poly"])
-async def get_poly_asset():
-    return 1
+@router.get("/assets/{asset}")
+async def get_poly_asset(asset : str):
+    r = requests.get(f'https://poly.googleapis.com/v1/assets/{asset}?key={data["poly_api_key"]}')
+    print(r.status_code)
+    if (r.status_code == 200):
+        return r.json()
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
