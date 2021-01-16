5d98fcbf6d8c60efb3a3331adda80fc9f246d429
Mike Nisbet <nisbet.mr@gmail.com>
2021-01-16 00:25:34 +0000

    CORS test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .poly import poly
 
 app = FastAPI(title="Icosa API", redoc_url=None)
 app.include_router(poly.router)
+
+origins = ["*"]
+
+app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
