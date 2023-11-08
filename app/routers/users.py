9cf4829d2d21fd3f28c9d5febde903c6615b36c7
Andy Baker <andy@andybak.net>
2023-11-08 15:55:33 +0000

    Avoid visually similar characters

--- app/routers/users.py
+++ app/routers/users.py
@@ -29,8 +29,10 @@ async def get_users_me(current_user: FullUser = Depends(get_current_user)):
     return current_user
 
 def generate_code(length=6):
-    characters = string.ascii_letters + string.digits
-    return ''.join(secrets.choice(characters) for i in range(length)).lower()
+    # Define a string of characters to exclude
+    exclude = 'I1O0'
+    characters = ''.join(set(string.ascii_letters + string.digits) - set(exclude))
+    return ''.join(secrets.choice(characters) for i in range(length)).upper()
 
 @router.get("/me/devicecode", response_model=DeviceCode)
 async def get_users_device_code(current_user: FullUser = Depends(get_current_user)):
