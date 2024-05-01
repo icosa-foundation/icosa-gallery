03bc43538b0c8652af1e5f8c0cddd35c5f01be76
James Rampton <james@rampton.io>
2024-05-01 14:12:16 +0100

    feat: add Django models to a basic admin

--- django/api/admin.py
+++ django/api/admin.py
@@ -1,3 +1,40 @@
+from api.models import (
+    Asset,
+    DeviceCode,
+    Oauth2Client,
+    Oauth2Code,
+    Oauth2Token,
+    User,
+)
+
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DeviceCode)
+class DeviceCodeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Oauth2Client)
+class Oauth2ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Oauth2Code)
+class Oauth2CodeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Oauth2Token)
+class Oauth2TokenAdmin(admin.ModelAdmin):
+    pass
--- django/icosa/settings.py
+++ django/icosa/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main_dashboard",
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Admin Tools settings
+
+ADMIN_TOOLS_INDEX_DASHBOARD = "main_dashboard.dashboard.MainDashboard"
