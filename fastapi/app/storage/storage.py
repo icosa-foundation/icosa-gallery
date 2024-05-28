b849c5aa56944329248e8bd9f2e3b7a42423b152
James Rampton <james@rampton.io>
2024-05-28 11:45:53 +0100

    chore: formatting

--- fastapi/app/storage/storage.py
+++ fastapi/app/storage/storage.py
@@ -1,15 +1,19 @@
 import json
+
 from google.cloud import storage
 
 with open("config.json") as config_file:
     data = json.load(config_file)
+
 
 async def upload_file_gcs(source_file, destination_blob_name):
     """Uploads a file to the bucket."""
     # source_file = "local/path/to/file"
     # destination_blob_name = "storage-object-name"
 
-    storage_client = storage.Client.from_service_account_json(data["service_account_data"])
+    storage_client = storage.Client.from_service_account_json(
+        data["service_account_data"]
+    )
     bucket = storage_client.bucket(data["gcloud_bucket_name"])
     blob = bucket.blob(destination_blob_name)
     try:
@@ -20,12 +24,15 @@ async def upload_file_gcs(source_file, destination_blob_name):
         return False
     return f'https://storage.googleapis.com/{data["gcloud_bucket_name"]}/{destination_blob_name}'
 
+
 async def upload_url_gcs(source_url, destination_blob_name):
     """Uploads a file from a url the bucket."""
     # source_url = "example.com/asset.json"
     # destination_blob_name = "storage-object-name"
 
-    storage_client = storage.Client.from_service_account_json(data["service_account_data"])
+    storage_client = storage.Client.from_service_account_json(
+        data["service_account_data"]
+    )
     bucket = storage_client.bucket(data["gcloud_bucket_name"])
     blob = bucket.blob(destination_blob_name)
     try:
@@ -36,9 +43,12 @@ async def upload_url_gcs(source_url, destination_blob_name):
         return False
     return f'https://storage.googleapis.com/{data["gcloud_bucket_name"]}/{destination_blob_name}'
 
+
 async def remove_file_gcs(file_blob):
     """Removes a file from the bucket."""
-    storage_client = storage.Client.from_service_account_json(data["service_account_data"])
+    storage_client = storage.Client.from_service_account_json(
+        data["service_account_data"]
+    )
     bucket = storage_client.bucket(data["gcloud_bucket_name"])
     blob = bucket.blob(file_blob)
     try:
@@ -48,14 +58,19 @@ async def remove_file_gcs(file_blob):
         return False
     return True
 
+
 async def remove_folder_gcs(folder_path):
     """Remove entire folder from bucket."""
-    storage_client = storage.Client.from_service_account_json(data["service_account_data"])
-    blobs = storage_client.list_blobs(data["gcloud_bucket_name"], prefix=folder_path)
+    storage_client = storage.Client.from_service_account_json(
+        data["service_account_data"]
+    )
+    blobs = storage_client.list_blobs(
+        data["gcloud_bucket_name"], prefix=folder_path
+    )
     for blob in blobs:
         try:
             blob.delete()
         except Exception as e:
-            print (e)
+            print(e)
             return False
     return True
