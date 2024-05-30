334bc00d8df694dba908b1499307abe4a83a4cbd
James Rampton <james@rampton.io>
2024-05-30 15:29:20 +0100

    fix: order home page sketches by reverse date

--- django/api/views.py
+++ django/api/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render
 def home(request):
     template = "home.html"
 
-    context = {"assets": Asset.objects.filter(visibility=PUBLIC)}
+    context = {
+        "assets": Asset.objects.filter(visibility=PUBLIC).order_by("-id")
+    }
     return render(
         request,
         template,
