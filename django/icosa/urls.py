a2aea149460a3bc77846d46646e49382bbe25672
James Rampton <james@rampton.io>
2024-06-05 08:56:25 +0100

    refactor: spilt out views

--- django/icosa/urls.py
+++ django/icosa/urls.py
@@ -1,20 +1,4 @@
-"""
-URL configuration for icosa project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from api import views as api_views
+from api.views import main as main_views
 
 from django.conf.urls import include
 from django.contrib import admin
@@ -23,6 +7,6 @@ from django.urls import path
 urlpatterns = [
     path("admin_tools/", include("admin_tools.urls")),
     path("admin/", admin.site.urls),
-    path("", api_views.home, name="home"),
-    path("user/<str:slug>/", api_views.user, name="user"),
+    path("", main_views.home, name="home"),
+    path("user/<str:slug>/", main_views.user, name="user"),
 ]
