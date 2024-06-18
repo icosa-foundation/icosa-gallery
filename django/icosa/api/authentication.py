6cc535179a73c21a5699dc408519f1c6147c1306
James Rampton <james@rampton.io>
2024-06-18 10:14:20 +0100

    style: reuse authentication error

--- django/icosa/api/authentication.py
+++ django/icosa/api/authentication.py
@@ -10,6 +10,7 @@ ALGORITHM = "HS256"
 
 class AuthBearer(HttpBearer):
     def authenticate(self, request, token):
+        authentication_error = HttpError(401, "Invalid Credentials")
         try:
             payload = jwt.decode(
                 token, settings.JWT_KEY, algorithms=[ALGORITHM]
@@ -17,12 +18,13 @@ class AuthBearer(HttpBearer):
             username: str = payload.get("sub")
             if username is None:
                 # headers={"WWW-Authenticate": "Bearer"},
-                raise HttpError(401, "Invalid Credentials")
+                raise authentication_error
         except jwt.PyJWTError:
             # headers={"WWW-Authenticate": "Bearer"},
-            raise HttpError(401, "Invalid Credentials")
+            raise authentication_error
         user = User.objects.get(email=username)
         if user is None:
             # headers={"WWW-Authenticate": "Bearer"},
-            raise HttpError(401, "Invalid Credentials")
+            raise authentication_error
+        print(user)
         return user
