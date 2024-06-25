92347b8e8c904a506f6aa3d780b21f47bf543cb9
James Rampton <james@rampton.io>
2024-06-25 14:51:27 +0100

    fix: better schema for Tags

--- django/icosa/models.py
+++ django/icosa/models.py
@@ -64,7 +64,7 @@ class User(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
