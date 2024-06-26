a7354b172a6ef399a796763f75fb5edcb33e4cc4
James Rampton <james@rampton.io>
2024-06-26 13:34:43 +0100

    chore: formatting

--- django/icosa/management/commands/gcp_to_b2.py
+++ django/icosa/management/commands/gcp_to_b2.py
@@ -3,7 +3,7 @@ import os
 
 from icosa.models import Asset
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
