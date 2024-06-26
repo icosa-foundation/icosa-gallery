ea1a2d108657a4dd31df58cbf7902a5be6560a28
James Rampton <james@rampton.io>
2024-06-26 13:11:22 +0100

    feat: add gcp to b2 migrator

---
+++ django/icosa/management/commands/gcp_to_b2.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+from icosa.models import Asset
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    help = """Swaps Assets' hard-coded storage bucket references from Google
+    Cloud Platform to Backblaze B2"""
+
+    def handle(self, *args, **options):
+        GCP_URL = os.environ.get("GCP_URL")
+        B2_URL = os.environ.get("B2_URL")
+        if not GCP_URL or not B2_URL:
+            print("must set GCP_URL and B2_URL in your .env file")
+            return
+
+        assets_with_formats = Asset.objects.filter(formats__icontains=GCP_URL)
+        assets_with_thumbnails = Asset.objects.filter(
+            thumbnail__icontains=GCP_URL
+        )
+
+        for asset in assets_with_formats:
+            format_str = json.dumps(asset.formats)
+            asset.formats = json.loads(format_str.replace(GCP_URL, B2_URL))
+            asset.save()
+
+        for asset in assets_with_thumbnails:
+            url = asset.thumbnail.name.replace(GCP_URL, "icosa/")
+            asset.thumbnail = url
+            asset.save()
