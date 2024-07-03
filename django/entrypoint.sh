3372514e8025d629494bbafc0a7470e6bbfd89a2
James Rampton <james@rampton.io>
2024-07-03 13:12:42 +0100

    fix: use asgi and fix path to run it

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 echo "Running in $DEPLOYMENT_ENV mode"
 if [[ $DEPLOYMENT_ENV == 'production' ]]; then
-    gunicorn recipe_book.wsgi:application --bind 0.0.0.0:8001 --timeout 900
+    gunicorn django_project.asgi:application --bind 0.0.0.0:8001 --timeout 900
 else
     python manage.py runserver 0.0.0.0:8001
 fi
