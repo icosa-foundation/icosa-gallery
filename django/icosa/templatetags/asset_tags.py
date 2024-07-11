5e31c6cea3eb31ec143ba58c9ed38bb23f415ac0
James Rampton <james@rampton.io>
2024-07-11 17:19:48 +0100

    fix: broken like button for logged out users

--- django/icosa/templatetags/asset_tags.py
+++ django/icosa/templatetags/asset_tags.py
@@ -8,5 +8,9 @@ register = template.Library()
 @register.inclusion_tag("main/tags/like_button.html")
 def like_button(request, asset):
     owner = IcosaUser.from_request(request)
+    if owner is not None:
+        is_liked = asset.id in owner.likes.values_list("id", flat=True)
+    else:
+        is_liked = False
 
-    return {"is_liked": asset.id in owner.likes.values_list("id", flat=True)}
+    return {"is_liked": is_liked}
