24288872100dceb564c19c90a1917bde11af601c
James Rampton <james@rampton.io>
2024-08-22 14:00:30 +0100

    Implement remove slash middleware

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "icosa.middleware.redirect.RemoveSlashMiddleware",
 ]
 
 ROOT_URLCONF = "django_project.urls"
