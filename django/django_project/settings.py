245af8e4aa12e457d8b90a72be1f959ba2335e23
James Rampton <james@rampton.io>
2024-08-29 16:10:49 +0100

    Remove huey import from settings

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import huey
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
