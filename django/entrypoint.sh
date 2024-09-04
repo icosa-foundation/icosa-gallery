64b9e9b618aeecbc3a6a5072b40f1dc38d5fbb09
James Rampton <james@rampton.io>
2024-09-04 11:42:52 +0100

    Automate site setup

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -2,8 +2,18 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+if [[ -v DEPLOYMENT_HOST_WEB ]];
+then
+    echo "Setting $DEPLOYMENT_HOST_WEB as the default domain"
+    python manage.py shell -c \
+    "from django.contrib.sites.models import Site; Site.objects.filter(domain='example.com').update(domain='$DEPLOYMENT_HOST_WEB', name='$DEPLOYMENT_HOST_WEB')"
+fi
+
+
 echo "Running in $DEPLOYMENT_ENV mode"
-if [[ $DEPLOYMENT_ENV == 'production' ]]; then
+if [[ $DEPLOYMENT_ENV == 'production' ]];
+then
     python manage.py run_huey &
     gunicorn django_project.wsgi:application --bind 0.0.0.0:8000 --timeout 900
 else
