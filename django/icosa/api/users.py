0d53bcf4f339aa8aa026711bc10ee6ddba3681fc
James Rampton <james@rampton.io>
2024-09-09 15:22:44 +0100

    Revert "Remove the poly category map; it's unused now"
    This reverts commit 66371b553273bfdee81d68e2e590b79312e7cb17.

--- django/icosa/api/users.py
+++ django/icosa/api/users.py
@@ -1,6 +1,10 @@
 from typing import List
 
-from icosa.api import COMMON_ROUTER_SETTINGS, AssetPagination
+from icosa.api import (
+    COMMON_ROUTER_SETTINGS,
+    POLY_CATEGORY_MAP,
+    AssetPagination,
+)
 from icosa.models import PRIVATE, PUBLIC, UNLISTED, Asset, Tag
 from icosa.models import User as IcosaUser
 from ninja import Query, Router
