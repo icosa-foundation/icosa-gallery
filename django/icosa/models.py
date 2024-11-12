a684b222920826c2470ff0e06fd8984393ec868d
Andy Baker <andy@andybak.net>
2024-11-12 02:00:03 +0000

    don't force obj for blocks models (temp)

--- django/icosa/models.py
+++ django/icosa/models.py
@@ -332,7 +332,8 @@ class Asset(models.Model):
         # Return early with an obj if we know the asset is a blocks file.
         # There are some issues with displaying GLTF files from Blocks so we
         # have to return an OBJ and its associated MTL.
-        if self.is_blocks:
+        # TODO: Bypass this temporarily for testing.
+        if False: # self.is_blocks:
             # TODO Prefer some roles over others
             # TODO error handling
             obj_format = self.polyformat_set.filter(format_type="OBJ").first()
