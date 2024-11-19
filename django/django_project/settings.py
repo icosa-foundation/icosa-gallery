9ed474f775a5c53974157825fc22b5a71372cec1
James Rampton <james@rampton.io>
2024-11-19 13:17:09 +0000

    Add gzip middleware

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.gzip.GZipMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
