e2e77d685c8954c0c4136e379f8290868f8151b4
James Rampton <james@rampton.io>
2025-01-21 13:12:56 +0000

    Update get_owner to use django_user

--- django/icosa/helpers/user.py
+++ django/icosa/helpers/user.py
@@ -2,11 +2,9 @@ from icosa.models import AssetOwner
 
 
 def get_owner(user):
-    email = getattr(user, "username", None)
     owner = None
-    if email:
-        try:
-            owner = AssetOwner.objects.get(email=email)
-        except (AssetOwner.DoesNotExist, AssetOwner.MultipleObjectsReturned):
-            pass
+    try:
+        owner = AssetOwner.objects.get(django_user=user)
+    except (AssetOwner.DoesNotExist, AssetOwner.MultipleObjectsReturned):
+        pass
     return owner
