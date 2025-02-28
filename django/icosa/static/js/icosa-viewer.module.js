49facd919d9613bba571a0b918d32d1cebb87aa8
Andy Baker <andy@andybak.net>
2025-02-28 15:23:17 +0000

    Scale huge models to within a 100 unit sphere instead of a 1 unit sphere

--- django/icosa/static/js/icosa-viewer.module.js
+++ django/icosa/static/js/icosa-viewer.module.js
@@ -51818,7 +51818,8 @@ class $3c43f222267ed54b$export$2ec4afd9b3c16a85 {
         // Compensate for insanely large models
         let radius = this.overrides?.geometryData?.stats?.radius;
         if (radius > 10000) {
-            let scale = 1 / radius;
+            let scale = 1 / radius; // Unit scale
+            scale += 100; // Add a little extra
             this.loadedModel.scale.set(scale, scale, scale);
         }
         this.scene.add(this.loadedModel);
