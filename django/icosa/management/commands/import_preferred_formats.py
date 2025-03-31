a81eb68e45736271113ce76a72678fb08a87036d
James Rampton <james@rampton.io>
2025-03-31 15:30:16 +0100

    Actually run the import code in the jsonl loop

--- django/icosa/management/commands/import_preferred_formats.py
+++ django/icosa/management/commands/import_preferred_formats.py
@@ -33,20 +33,25 @@ class Command(BaseCommand):
                 print(f"Importing {asset_url}")
             asset = Asset.objects.get(url=asset_url)
 
-            # NOTE: this get assumes there are no duplicate roles in this data
-            # set at this time.
-            format = asset.format_set.get(role=json_line["role"])
-            format.format_type = json_line["format_type"]
-            format.root_resource.file = json_line["root_resource"]
-            existing_resources = list(format.resource_set.all())
-            for resource_file_name in json_line["resources"]:
-                filename = os.path.basename(resource_file_name)
-                # This should hard fail instead of choosing the first one
-                resource_obj = [x for x in existing_resources if str(x.file).endswith(filename)][0]
-                resource_obj.file = resource_file_name
-                resource_obj.save()
-            format.save()
-            # We are saving this to correctly set is_viewer_compatible.
-            asset.save(update_timestamps=False)
-            # TODO remove this return, it's for safety
-            return
+                # Skip if is_viewer_compatible is true; we either don't need to
+                # operate on this file, or we already have in a previous run.
+                if asset.is_viewer_compatible:
+                    continue
+
+                # NOTE: this get assumes there are no duplicate roles in this data
+                # set at this time.
+                format = asset.format_set.get(role=json_line["role"])
+                format.format_type = json_line["format_type"]
+                format.root_resource.file = json_line["root_resource"]
+                existing_resources = list(format.resource_set.all())
+                for resource_file_name in json_line["resources"]:
+                    filename = os.path.basename(resource_file_name)
+                    # This should hard fail instead of choosing the first one
+                    resource_obj = [x for x in existing_resources if str(x.file).endswith(filename)][0]
+                    resource_obj.file = resource_file_name
+                    resource_obj.save()
+                format.save()
+                # We are saving this to correctly set is_viewer_compatible.
+                asset.save(update_timestamps=False)
+                # TODO remove this return, it's for safety
+                return
