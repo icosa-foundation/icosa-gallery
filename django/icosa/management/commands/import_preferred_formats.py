df22c2aa67cb1288eb6f0c203144f80578b31da8
James Rampton <james@rampton.io>
2025-03-31 15:37:11 +0100

    Variable names

--- django/icosa/management/commands/import_preferred_formats.py
+++ django/icosa/management/commands/import_preferred_formats.py
@@ -27,11 +27,13 @@ class Command(BaseCommand):
                 format = asset.format_set.get(role=json_line["role"])
                 format.format_type = json_line["format_type"]
                 format.root_resource.file = json_line["root_resource"]
-                existing_resources = list(format.resource_set.all())
+                resources = list(format.resource_set.all())
                 for resource_file_name in json_line["resources"]:
                     filename = os.path.basename(resource_file_name)
+                    # Find the resource object we want to operate on based on
+                    # the filename we have in the jsonl.
                     # This should hard fail instead of choosing the first one
-                    resource_obj = [x for x in existing_resources if str(x.file).endswith(filename)][0]
+                    resource_obj = [x for x in resources if str(x.file).endswith(filename)][0]
                     resource_obj.file = resource_file_name
                     resource_obj.save()
                 format.save()
