331767710a356050e5d3c9f1614e67e3dcfeb32b
Andy Baker <andy@andybak.net>
2025-04-02 13:03:02 +0100

    Initial WIP for merge_owners

---
+++ django/icosa/management/commands/merge_owners.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+from icosa.models import Asset
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        icosa_assets = Asset.objects.filter(imported_from=None)
+
+        owner_matches = set()
+
+        for icosa_asset in icosa_assets:
+            if not icosa_asset.polydata: continue
+            poly_url = icosa_asset.polydata['name'].split('/')[-1]
+            poly_asset = Asset.objects.filter(url=poly_url).first()
+            owner_match = (icosa_asset.owner, poly_asset.owner)
+            owner_matches.add(owner_match)
+            print(icosa_asset.name, poly_asset.name)
+        print()
+        for owner_match in owner_matches:
+            icosa_owner, poly_owner = owner_match
+            print(icosa_owner.displayname, poly_owner.displayname)
