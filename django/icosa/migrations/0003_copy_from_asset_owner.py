05bb21b78b808a58cf4b1be55774c2fda346bc3d
James Rampton <james@rampton.io>
2025-04-14 15:32:51 +0100

    Formatting

--- django/icosa/migrations/0003_copy_from_asset_owner.py
+++ django/icosa/migrations/0003_copy_from_asset_owner.py
@@ -2,6 +2,7 @@
 
 from django.db import migrations
 
+
 def copy_likes(apps, schema_editor):
     OwnerAssetLike = apps.get_model("icosa", "OwnerAssetLike")
     UserLike = apps.get_model("icosa", "UserLike")
@@ -11,7 +12,8 @@ def copy_likes(apps, schema_editor):
             asset=asset_owner_like.asset,
             user=asset_owner_like.user.django_user,
         )
-        
+
+
 def copy_displayname(apps, schema_editor):
     AssetOwner = apps.get_model("icosa", "AssetOwner")
     for asset_owner in AssetOwner.objects.all():
@@ -19,16 +21,13 @@ def copy_displayname(apps, schema_editor):
             asset_owner.django_user.displayname = asset_owner.displayname
             asset_owner.django_user.save()
 
+
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('icosa', '0002_alter_user_options_user_displayname_alter_user_table_and_more'),
+        ("icosa", "0002_alter_user_options_user_displayname_alter_user_table_and_more"),
     ]
-
 
     operations = [
         migrations.RunPython(copy_likes),
         migrations.RunPython(copy_displayname),
     ]
-
-        
