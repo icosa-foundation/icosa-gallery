bf2c10e6bbcfaa862c13ba3fa3f6c02b238f052c
James Rampton <james@rampton.io>
2025-07-28 17:06:28 +0100

    Add exporter

---
+++ django/icosa/import_export/exporter.py
@@ -0,0 +1,90 @@
+import json
+
+from django.core.serializers import serialize
+from django.db.models import Model, Q
+from django.utils import timezone
+from icosa.models import Asset
+
+# TODO: Don't hard-code this
+STORAGE_ROOT = "https://f005.backblazeb2.com/file/icosa-gallery/"
+
+
+# Django's model_to_dict does not serialize all field types. This method gets
+# us more of the way there.
+def obj_to_dict(obj: Model) -> dict:
+    return json.loads(serialize("json", [obj]))[0]["fields"]
+
+
+def prefix_storage_root(s: str) -> str:
+    if s is None or s == "":
+        return s
+    return f"{STORAGE_ROOT}{s}"
+
+
+def export_assets(
+    asset_ids: list[int] = [],
+    owner_ids: list[int] = [],
+    user_ids: list[int] = [],
+):
+    q = Q()
+    if asset_ids:
+        q &= Q(id__in=asset_ids)
+    if owner_ids:
+        q &= Q(asset_owner__id__in=owner_ids)
+    if user_ids:
+        q &= Q(asset_owner__django_user__id__in=user_ids)
+    assets = Asset.objects.filter(q)
+
+    output = []
+
+    print(f"todo: {assets.count()} assets.")
+    for i, asset in enumerate(assets.iterator(chunk_size=1000)):
+        if i and i % 1000 == 0:
+            print(f"done {i}")
+
+        # TODO: We need to be careful of `remix_ids` on the Asset. These
+        # IDs will be the source system's IDs and might either not be
+        # present in the destination, or will be the wrong assets. Should we
+        # blank this field, or export full Assets listed here? Potential for export
+        # size to explode if we do the latter. Perhaps this is user-selectable.
+        asset_data = obj_to_dict(asset)
+
+        # We need the full image url on the remote server, not just the path.
+        asset_image_keys = ["thumbnail", "preview_image"]
+        for key in asset_image_keys:
+            asset_data[key] = prefix_storage_root(asset_data[key])
+
+        formats = asset.format_set.all()
+        format_set_data = []
+        for format in formats:
+            format_data = obj_to_dict(format)
+            del format_data["asset"]
+            if format.root_resource:
+                format_data["root_resource"] = obj_to_dict(format.root_resource)
+            else:
+                format_data["root_resource"] = None
+            format_data["resource_set"] = []
+            for resource in format.resource_set.all():
+                resource_data = obj_to_dict(resource)
+                del resource_data["asset"]
+                resource_data["file"] = prefix_storage_root(resource_data["file"])
+                format_data["resource_set"].append(resource_data)
+            format_set_data.append(format_data)
+        asset_data.update({"format_set": format_set_data})
+
+        # TODO: This will not de-duplicate owners and users. I think this is ok for
+        # a naive import of everything, but if this turns out to be very
+        # wasteful, I should rethink this. We will need a more clever way of
+        # making sure the right assets get the right owners, etc.
+        if asset.owner:
+            owner_data = obj_to_dict(asset.owner)
+            if asset.owner.django_user:
+                # TODO: This exports the django user's password hash. Not
+                # as terrible as plain text, but perhaps we should warn the
+                # operating user that we are doing this.
+                owner_data["django_user"] = obj_to_dict(asset.owner.django_user)
+            asset_data["owner"] = owner_data
+        output.append(asset_data)
+
+    with open(f"asset_export{timezone.now().strftime('%d-%m-%y_%H-%M-%S')}.json", "w") as f:
+        json.dump(output, f, ensure_ascii=False, indent=4)
---
+++ django/icosa/management/commands/export_user_assets.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+from icosa.import_export.exporter import export_assets
+
+User = get_user_model()
+
+STORAGE_ROOT = "https://f005.backblazeb2.com/file/icosa-gallery/"
+
+
+class Command(BaseCommand):
+    help = """Exports data for import into another instance of Icosa Gallery."""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--asset-ids",
+            nargs="*",
+            default=[],
+            type=int,
+            help="Space-separated list of Asset ids to export. If blank, will export all Assets. Use in conjunction with --owner-ids or --user-ids to further limit exported Assets.",
+        )
+        parser.add_argument(
+            "--owner-ids",
+            nargs="*",
+            default=[],
+            type=int,
+            help="Space-separated list of AssetOwner ids whose Assets you wish to export. If blank, will export all assets. Use in conjunction with --user-ids or --asset-ids to further limit exported Assets.",
+        )
+        parser.add_argument(
+            "--user-ids",
+            nargs="*",
+            default=[],
+            type=int,
+            help="Space-separated list of User ids to find s you wish to export. If blank, will export all assets. Use in conjunction with --asset-ids or --owner-ids to further limit exported Assets.",
+        )
+
+    def handle(self, *args, **options):
+        asset_ids = options.get("asset_ids", [])
+        owner_ids = options.get("owner_ids", [])
+        user_ids = options.get("user_ids", [])
+
+        export_assets(
+            asset_ids=asset_ids,
+            owner_ids=owner_ids,
+            user_ids=user_ids,
+        )
