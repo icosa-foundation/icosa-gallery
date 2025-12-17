f9f6d8cf01eda79cd59ce1f22fbb88409a4f0a6d
James Rampton <james@rampton.io>
2025-12-17 16:55:28 +0000

    List individual file downloads if we can't make a zip archive

--- django/icosa/models/format.py
+++ django/icosa/models/format.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from django.db import models
 from django.db.models import Q
@@ -42,14 +42,25 @@ class Format(models.Model):
         resource.save()
         self.save()
 
-    def get_all_resources(self, query: Q = Q()):
+    def get_resources(self, query: Q = Q(), exclude_q: Optional[Q] = None):
         resources = self.resource_set.filter(query)
+        if exclude_q is not None:
+            resources = resources.exclude(exclude_q)
         if self.root_resource:
             # We can only union on another queryset, even though we just want one
             # instance.
             root_resource = Resource.objects.filter(pk=self.root_resource.pk)
             resources = resources.union(root_resource)
         return resources
+
+    def get_all_resources(self, query: Q = Q()):
+        return self.get_resources(query)
+
+    def get_non_image_resources(self, query: Q = Q()):
+        exclude_q = Q()
+        for ext in [".png", ".jpg", ".jpeg"]:
+            exclude_q |= Q(file__endswith=ext)
+        return self.get_resources(query, exclude_q)
 
     def get_resource_data(self, resources: List[Resource]):
         if all([x.is_cors_allowed and x.remote_host for x in resources]):
