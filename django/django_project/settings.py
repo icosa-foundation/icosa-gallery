0f3e4e7665a39a5fa7d6afa9dde7f859d73345b8
James Rampton <james@rampton.io>
2026-02-05 14:32:50 +0000

    Rename the docker services to their actual application names
    Debatable, but this feels like it makes more sense than naming them after
    their roles.

--- django/django_project/settings.py
+++ django/django_project/settings.py
@@ -267,7 +267,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "HOST": os.environ.get("POSTGRES_HOST", "postgres"),
         "PORT": 5432,
     }
 }
--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 UP_TEST_RETRIES=10
-until bash -c '(echo > /dev/tcp/db/5432) > /dev/null 2>&1' || [ $UP_TEST_RETRIES -eq 0 ]; do
+until bash -c '(echo > /dev/tcp/postgres/5432) > /dev/null 2>&1' || [ $UP_TEST_RETRIES -eq 0 ]; do
     echo "Waiting for Postgres server, $((UP_TEST_RETRIES--)) remaining attempts..."
     sleep 1
 done
