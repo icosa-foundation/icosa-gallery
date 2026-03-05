bca3d88acf0408647f8f296a8f97bb9177dd1bef
James Rampton <james@rampton.io>
2026-03-05 14:43:42 +0000

    Send moderation email to admins if there are old items to moderate

--- django/icosa/models/moderation.py
+++ django/icosa/models/moderation.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
@@ -58,25 +59,47 @@ class ModerationNotification(models.Model):
         # being installed
         User = get_user_model()
 
-        cutoff_time = timezone.now() - datetime.timedelta(minutes=NOTIFICATION_PERIOD_MINUTES)
-        notifications = cls.objects.filter(
-            sent__gte=cutoff_time,
-        )
+        now = timezone.now()
+        cutoff_time = now - datetime.timedelta(minutes=NOTIFICATION_PERIOD_MINUTES)
+        notifications = cls.objects.filter(sent__gte=cutoff_time)
 
         # We have sent a digest within the notification period; give up for now.
         if notifications:
             return
 
+        objects_to_moderate = get_objects_to_moderate()
+        obj_count = objects_to_moderate.count()
+
+        # There is nothing to review; give up for now.
+        if obj_count == 0:
+            return
+
+        is_urgent = False
+        last_obj = objects_to_moderate.fetch_one(last=True)
+        if last_obj is not None:
+            timestamp = (
+                last_obj.moderation_state_change_time
+                if last_obj.moderation_state_change_time
+                else last_obj.update_time
+                if last_obj.update_time
+                else last_obj.create_time
+            )
+            if timestamp < cutoff_time:
+                is_urgent = True
+
         current_site = Site.objects.get_current()
-        obj_count = get_objects_to_moderate().count()
         subject = f"Icosa Gallery - moderation required for {obj_count} item{pluralize(obj_count)}"
+        if is_urgent:
+            subject = f"URGENT! {subject}"
         html = f"<p>Please go to <a href='https://{current_site.domain}{reverse('icosa:moderation_queue')}'>the moderation queue</a> and approve or reject the latest changes.</p>"
         html = f"<html><body>{html}</body></html>"
 
         moderators = User.objects.filter(groups__name__in=["Moderator"])
-        if not moderators:
-            return
         recipients = [x.email for x in moderators]
+        if is_urgent:
+            admin_email = getattr(settings, "ADMIN_EMAIL", None)
+            if admin_email is not None:
+                recipients.append(admin_email)
 
         spawn_send_html_mail(subject, html, recipients)
 
